503fe340dd1a631ca602575ada5fa6a21e3a0ce9
natashabaudelaire <200302287+NatashaBaudelaire@users.noreply.github.com>
2026-09-15 10:14:59 -0300

    refactor: export cog classes from cogs package

---
+++ cogs/__init__.py
@@ -0,0 +1,8 @@
+from cogs.basic import Basic
+from cogs.study import Study
+from cogs.profile import Profile
+from cogs.training import Training
+from cogs.ranking import Ranking
+from cogs.history import History
+
+__all__ = ["Basic", "Study", "Profile", "Training", "Ranking", "History"]
